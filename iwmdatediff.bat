ce77caabe2cec728ed12d4bc21f08d18b7909bd8
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2024-03-11 12:00:36 +0900

    Add files via upload

--- iwmdatediff.bat
+++ iwmdatediff.bat
@@ -1,50 +1,44 @@
-:: Ini ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-	@echo off
-	cls
+@echo off
+cls
 
-	:: ファイル名はソースと同じ
-	set fn=%~n0
-	set src=%fn%.c
-	set fn_exe=%fn%.exe
-	set cc=gcc.exe
-	set cc_op=-Os -Wall -lgdi32 -luser32 -lshlwapi
-	set lib=lib_iwmutil2.a
+:: ファイル名はソースと同じ
+set fn=%~n0
+set src=%fn%.c
+set fn_exe=%fn%.exe
+set cc=gcc.exe
+set lib=lib_iwmutil2.a
+set op_link=-Os -Wall -Wextra -lgdi32 -luser32 -lshlwapi
 
-:: Make ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
+:: Assembler
 ::	echo --- Compile -S ------------------------------------
+::	cp -f %fn%.s %fn%.s.old
 ::	for %%s in (%src%) do (
-::		%cc% %%s -S %cc_op%
+::		%cc% %%s -S %op_link%
 ::		echo %%~ns.s
 ::	)
 ::	echo.
 
+:: Make
 	echo --- Make ------------------------------------------
-	for %%s in (%src%) do (
-		echo %%s
-		%cc% %%s -c -Wall %cc_op%
-	)
-	%cc% *.o %lib% -o %fn_exe% %cc_op%
-	echo.
-
-	:: 後処理
+	%cc% %src% %lib% %op_link% -o %fn_exe%
 	strip %fn_exe%
-	rm *.o
-
-	:: 失敗
-	if not exist "%fn_exe%" goto end
-
-	:: 成功
 	echo.
+
+:: Dump
+::	cp -f %fn%.objdump %fn%.objdump.old
+::	objdump -d -s %fn_exe% > %fn%.objdump
+::	echo.
+
+:: Test
 	pause
+	chcp 65001
 	cls
 
-:: Test ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+	%fn_exe%
 
 	set s1="1970/12/10"
 	set s2=now
 
-	%fn_exe%
 	%fn_exe% %s2% %s1%
 	%fn_exe% %s2% %s1% -format="%%g%%y年%%m月%%d日 %%D通算日 %%W週%%w日"
 	%fn_exe% %s1% %s2%
@@ -57,8 +51,7 @@
 	echo jd . -f="g:%%g M:%%M D:%%D H:%%H N:%%N S:%%S W:%%W"
 	%fn_exe% jd . -f="g:%%g M:%%M D:%%D H:%%H N:%%N S:%%S W:%%W"
 
-:: Quit ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:end
+:: Quit
 	echo.
 	pause
 	exit
